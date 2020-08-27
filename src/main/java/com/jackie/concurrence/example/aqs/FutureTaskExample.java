c4ace161e4e4ba2e3f7248c07c4ea1ff4261acf3
JackieXu <343141195@qq.com>
2020-08-27 16:16:42 +0800

    future

---
+++ src/main/java/com/jackie/concurrence/example/aqs/FutureTaskExample.java
@@ -0,0 +1,35 @@
+package com.jackie.concurrence.example.aqs;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
+
+/**
+ * Created with IntelliJ IDEA
+ * Description:
+ *
+ * @author xujj
+ * @date 2020-08-26
+ */
+@Slf4j
+public class FutureTaskExample {
+
+    public static void main(String[] args) throws Exception {
+        FutureTask<String> futureTask = new FutureTask<>(
+                new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                log.info("do something in callable");
+                Thread.sleep(5000);
+                return "Done";
+            }
+        });
+
+        new Thread(futureTask).start();
+        log.info("do something in main");
+        Thread.sleep(1000);
+        String result = futureTask.get();
+        log.info("result: {}", result);
+    }
+}
